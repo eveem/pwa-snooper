e8db8d06890879480a83e41bd7dbaa287831d5d3
tidjungs <waewprach.s@ku.th>
2017-06-24 22:13:16 +0700

    merge

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  state = {
+    count: 0
+  }
   render() {
     return (
       <div>
-        <h1>Login</h1>
-        <h2>Test CI</h2>
+        <h1>{ this.state.count }</h1>
+        <button onClick={() => {
+          this.setState({
+            count: this.state.count + 1
+          })
+        }}>add</button>
       </div>
     )
   }
