e5b2235a19a2e1ff9b701a56081340ac8a7270a5
eveem <monpriya.tammavong@gmail.com>
2017-06-24 23:50:53 +0700

    Add input

--- src/component/Create.js
+++ src/component/Create.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Input } from 'antd'
 
 class Create extends Component {
+  state = { 
+  	user_id: '',
+  	text: ''}
+
+  onclick = () => {
+  	this.setState({ text: this.state.text })
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value })
+    console.log(e.target.value)
+  }
+
   render() {
     return (
       <div>
         <h1>Create</h1>
+        <Input type="textarea" value={this.state.text} onChange={this.handleChange} placeholder="Tell me your secret." />
+        <button onClick={this.onclick}>click</button>
       </div>
     )
   }
