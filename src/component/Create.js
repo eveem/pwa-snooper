2baebb4e965436557f70ea104f0e5b1ec12eb95c
eveem <monpriya.tammavong@gmail.com>
2017-06-25 01:32:56 +0700

    Write data to firebase complete

--- src/component/Create.js
+++ src/component/Create.js
@@ -2,47 +2,48 @@ import React, { Component } from 'react'
 import { Input } from 'antd'
 import firebase from 'firebase'
 import { connect } from 'react-firebase'
+import { uniqueId } from 'lodash'
 
 firebase.initializeApp({
   databaseURL: 'https://pwa-crossword.firebaseio.com/'
 })
 
-const Counter = ({ value, setValue }) => (
-  <div>
-    <button onClick={() => setValue(value - 1)}>-</button>
-    <span>{value}</span>
-    <button onClick={() => setValue(value + 1)}>+</button>
-  </div>
-)
- 
-export default connect((props, ref) => ({
-  value: 'counterValue',
-  setValue: value => ref('counterValue').set(value)
-}))(Counter)
+var database = firebase.database();
 
-// class Create extends Component {
-//   state = { 
-//   	user_id: '',
-//   	text: ''}
+class Create extends Component {
+  state = { 
+  	text: '',
+  	id: ''
+  }
 
-//   onclick = () => {
-//   	this.setState({ text: this.state.text })
-//   }
+  writePost = (post_id, text, timestamp) => {
+  	this.state.id = uniqueId()
+  	firebase.database().ref('post/' + this.state.id).set({
+  		post_id: this.state.id,
+  		text: this.state.text,
+  		timestamp: firebase.database.ServerValue.TIMESTAMP
+  	})
+  }
+  	
+  onclick = () => {
+  	this.setState({ text: this.state.text })
+  	this.writePost ()
+  }
 
-//   handleChange = e => {
-//     this.setState({ text: e.target.value })
-//     console.log(e.target.value)
-//   }
+  handleChange = e => {
+    this.setState({ text: e.target.value })
+    console.log(e.target.value)
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Create</h1>
-//         <Input type="textarea" value={this.state.text} onChange={this.handleChange} placeholder="Tell me your secret." />
-//         <button onClick={this.onclick}>click</button>
-//       </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h1>Create</h1>
+        <Input type="textarea" value={this.state.text} onChange={this.handleChange} placeholder="Tell me your secret." />
+        <button onClick={this.onclick}>click</button>
+      </div>
+    )
+  }
+}
 
-// export default Create
+export default Create
